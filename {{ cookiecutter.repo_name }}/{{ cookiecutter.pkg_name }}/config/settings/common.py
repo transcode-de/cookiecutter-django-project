825cc2abcd9e6ac36c500946f967e9e73b1ac7a5
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-20 22:58:16 +0100

    Introduced AdminsValue
    values.TupleValue does not work for the ADMINS setting, because ADMINS must be
    a tuple of tuples. Therefore values.SingleNestedTupleValue must be used.
    
    The new AdminsValue extends values.SingleNestedTupleValue and validates in
    addition the contents of the outer tuple and the inner tuples.

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/settings/common.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/settings/common.py
@@ -1,6 +1,42 @@
 import os
 
 from configurations import Configuration, values
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+
+
+class AdminsValue(values.SingleNestedTupleValue):
+    """A ``SingleNestedTupleValue`` subclass to be used for the ADMINS and MANAGERS settings.
+
+    Two validators are executed for each tuple:
+
+        1. The exact length of each tuple must be two.
+        2. The second element of each tuple must be a valid email address.
+    """
+    def __init__(self, *args, **kwargs):
+        super(AdminsValue, self).__init__(*args, **kwargs)
+        if self.default:
+            self.validate(self.default)
+
+    def validate_length(self, value):
+        if len(value) != 2:
+            raise ValueError('Each ADMINS tuple must have exact two values')
+
+    def validate_email(self, value):
+        try:
+            validate_email(value)
+        except ValidationError:
+            raise ValueError('Cannot interpret email value {0!r}'.format(value))
+
+    def validate(self, value):
+        for item in value:
+            self.validate_length(item)
+            self.validate_email(item[1])
+
+    def to_python(self, value):
+        value = super(AdminsValue, self).to_python(value)
+        self.validate(value)
+        return value
 
 
 class BaseDir(object):
@@ -22,8 +58,8 @@ class Common(Configuration):
 
     DEBUG = values.BooleanValue(False)
 
-    ADMINS = values.TupleValue(
-        ('{{ cookiecutter.author_name }}', '{{ cookiecutter.error_email }}'),
+    ADMINS = AdminsValue(
+        (('{{ cookiecutter.author_name }}', '{{ cookiecutter.error_email }}'),)
     )
     MANAGERS = ADMINS
 
