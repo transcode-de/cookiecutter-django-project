400844eaf160990248e3f059f9a6572ad86a1f2e
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-25 03:07:53 +0100

    Added docstring for AppConfig template class

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/app_template/apps.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/app_template/apps.py
@@ -3,5 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class {{ '{{ app_name|title }}' }}Config(AppConfig):
+    """Configuration for {{ '{{ app_name }}' }} app."""
+
     name = '{{ cookiecutter.pkg_name }}.apps.{{ '{{ app_name }}' }}'
     verbose_name = _("{{ '{{ app_name|title }}' }}")
