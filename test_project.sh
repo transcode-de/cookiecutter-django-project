ccb61dc84930fb3c9aa299012a721ca46d026d01
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-27 14:06:13 +0100

    Extended project tox env to test database and dev env

--- test_project.sh
+++ test_project.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 cd my-project
+
+# Create Git repository, add all files and commit
 git init
 git config user.email "alice@example.com"
 git config user.name "Alice Brown"
 git add -A .
 git commit -m 'Initial commit'
+
+# Run tox
 tox
+
+# Set up the development environment
+make develop
+PG_UUID=`faker uuid4`
+if [ "$CI" = true ] ; then
+    PG_CREATE_USER="-U postgres"
+else
+    PG_CREATE_USER=
+fi
+createuser -d -e $PG_UUID $PG_CREATE_USER
+echo $PG_UUID > envs/dev/PGPASSWORD
+echo "postgres://$PG_UUID:$PG_UUID@localhost/$PG_UUID" > envs/dev/DEFAULT_DATABASE_URL
+
+# Run the tests and collect coverage data
+cat >> tests/test_context_processors.py <<EOF
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_db(admin_user):
+    assert admin_user.is_superuser
+EOF
+make coverage
+
+# Clean up
+dropuser -e $PG_UUID
