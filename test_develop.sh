f5b9a2d6150b56e499baef8f454e526b51350946
Markus Zapke-Gründemann <markus@transcode.de>
2015-11-29 14:39:36 +0100

    Split project tox env into sevaral envs

--- test_develop.sh
+++ test_develop.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 cd my-project
 
-# Create Git repository, add all files and commit
-git init
-git config user.email "alice@example.com"
-git config user.name "Alice Brown"
-git add -A .
-git commit -m 'Initial commit'
-
-# Run tox
-tox
-
 # Set up the development environment
 make develop
 PG_UUID=`faker uuid4`
