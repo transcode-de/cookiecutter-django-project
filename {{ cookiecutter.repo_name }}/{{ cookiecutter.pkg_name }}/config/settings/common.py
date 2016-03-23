c18577036be90fabea43c576b75c7574bdc90219
Max Brauer <max@transcode.de>
2016-03-23 15:07:12 +0100

    Added rules.apps.AutodiscoverRulesConfig to INSTALLED_APPS.

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/settings/common.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/settings/common.py
@@ -183,6 +183,7 @@ class Common(Configuration):
         'django.contrib.admin',
         'django.contrib.admindocs',
         'crispy_forms',
+        'rules.apps.AutodiscoverRulesConfig',
     )
 
     CACHES = values. DictValue({
