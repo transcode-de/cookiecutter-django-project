07c995942e0ebb7b30d23769e03fb0f6b88c33ec
Markus Zapke-Gründemann <markus@transcode.de>
2016-03-24 20:41:46 +0100

    Added copy or link functionality to npm_install.sh
    The script does now either copy or link the node_modules cache.

--- npm_install.sh
+++ npm_install.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
 #
 # Use a single node_modules directory for all tox envs.
+# Either copy or link the node_modules cache.
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
+E_NOARGS=75
 NODE_CACHE=../../node_cache
 TOXTMP=$(pwd)
+
+function usage {
+    echo "Usage: `basename ${0}` [copy | link]"
+    exit ${E_NOARGS}
+}
+
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+case $1 in
+    copy) CMD="cp -R";;
+    link) CMD="ln -s";;
+    *) usage;;
+esac
 
 if [ ! -d ${NODE_CACHE} ]; then
     mkdir ${NODE_CACHE}
@@ -17,4 +34,4 @@ if [ ! -d ${NODE_CACHE} ]; then
 fi
 
 cd ${TOXTMP}/my-project
-ln -fs ../${NODE_CACHE}/node_modules
+${CMD} ../${NODE_CACHE}/node_modules .
