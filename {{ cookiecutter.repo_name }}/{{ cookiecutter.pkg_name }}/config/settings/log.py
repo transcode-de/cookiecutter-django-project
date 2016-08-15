11d242f8ddb544a3fb5964995155326de6e8d987
René Muhl <rene@transcode.de>
2016-08-15 17:07:31 +0200

    Removed settings logging module

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.pkg_name }}/config/settings/log.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-"""Filter functions for python logging framework."""
-
-
-def RequireMailAdminsTrue():
-    """Return MAIL_ADMINS setting."""
-    return settings.MAIL_ADMINS
